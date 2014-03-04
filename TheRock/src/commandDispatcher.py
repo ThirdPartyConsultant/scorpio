59d9df69b02a8b1492a4b3401c1d4dddb3b0ce71
Tao-Sheng <taosheng.chen@gmail.com>
2014-03-04 02:59:58 +0000

    control flag

--- TheRock/src/commandDispatcher.py
+++ TheRock/src/commandDispatcher.py
@@ -4,7 +4,7 @@ import time
 
 class CommandDispatcher:
     def __init__(self):
-        self.executionDict = {'open_the_gate':'','turn_on_light':self._turn_on_light,'turn_off_light':self._turn_off_light}
+        self.executionDict = {'open_the_gate':'','turn_on_light':self._turn_on_light,'turn_off_light':self._turn_off_light,'turn_on_rely':self._turn_on_rely,'turn_off_rely':self._turn_off_rely}
 
 
     def isAction(self, action):
@@ -13,7 +13,7 @@ class CommandDispatcher:
     def execute(self, action):
         try:
             self.executionDict[action]()
-            return "done"
+            return "done" + action
         except:
             return "somethine wrong", sys.exc_info()[0]
             
@@ -23,9 +23,23 @@ class CommandDispatcher:
         GPIO.output(18, False)
 
     def _turn_off_light(self):
-        GPIO.setmode(GPIO.BOARD)
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(18, GPIO.OUT)
         GPIO.output(18, True)
+
+    def _turn_on_rely(self):
+        GPIO.setwarnings(True)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(24, GPIO.OUT)
+        GPIO.output(24, GPIO.LOW)
+
+    def _turn_off_rely(self):
+        GPIO.setwarnings(True)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(24, GPIO.OUT)
+        GPIO.output(24, GPIO.HIGH)
+
+
 
 
 #c = CommandDispatcher()
